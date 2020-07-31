ac7ecfab0b62adfca8c0f68b4c2c11115cc3adf0
Ramesh kumar <42023815+ramesh-kumarjha@users.noreply.github.com>
2020-07-31 16:17:38 +0530

    Add files via upload

---
+++ json.py
@@ -0,0 +1,12 @@
+import simplejson as json
+
+with open('data.json', 'r') as f:
+        json_text = f.read()
+
+# Decode the JSON string into a Python dictionary.
+apod_dict = json.loads(json_text)
+print(apod_dict['explanation'])
+
+# Encode the Python dictionary into a JSON string.
+new_json_string = json.dumps(apod_dict, indent=4)
+print(new_json_string)
